374b92c03abaf83b1cd002410c30689caaac815b
ej980 <mike9801215@gmail.com>
2024-09-26 16:56:40 +0800

    fix rack01

--- app.rb
+++ app.rb
@@ -14,14 +14,6 @@ use Rack::Cors do
   end
 end
 
-options '*' do
-  response.headers['Access-Control-Allow-Origin'] = '*'
-  response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
-  response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
-  200
-end
-
-
 set :port, ENV['PORT'] || 4567  
 
 get '/' do
